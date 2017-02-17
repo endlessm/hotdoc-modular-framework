a372169beee223f8adac30f102fc79a78403e211
Philip Chimento <philip@endlessm.com>
2017-02-16 17:42:16 -0800

    extension: Add sources and index arguments
    Adds command line arguments to Hotdoc with which to specify sources for
    processing by the modular framework extension, and for generating a smart
    index for modular framework modules.
    
    https://phabricator.endlessm.com/T14916

--- hotdoc_modular_framework/extension.py
+++ hotdoc_modular_framework/extension.py
@@ -9,13 +9,16 @@ app-description YAML.
 
 class HmfExtension(extension.Extension):
     extension_name = 'hotdoc_modular_framework'
+    argument_prefix = 'mf'
 
     def __init__(self, app, project):
         super().__init__(app, project)
 
     @staticmethod
     def add_arguments(parser):
-        parser.add_argument_group('Modular framework', _DESCRIPTION)
+        group = parser.add_argument_group('Modular framework', _DESCRIPTION)
+        HmfExtension.add_index_argument(group)
+        HmfExtension.add_sources_argument(group)
 
 
 def get_extension_classes():
