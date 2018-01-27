5baf1784c9c228f115dc9f14d9ec29dd88d9603e
Philip Chimento <philip@endlessm.com>
2018-01-26 18:25:52 -0800

    introspector: Add a mechanism to blacklist certain properties
    We want to prevent certain properties from appearing in certain modules.
    For example, in general we want 'expand' to appear on widget modules, but
    not on Window.Simple because it doesn't make sense to put it on the whole
    window.
    
    https://phabricator.endlessm.com/T11156

--- hotdoc_modular_framework/introspector.py
+++ hotdoc_modular_framework/introspector.py
@@ -4,8 +4,9 @@ from . import symbol, util
 _WHITELIST_TYPES = ['Number', 'String', 'Boolean',
     'GtkAlign', 'GtkJustification', 'GtkOrientation', 'GtkStackTransitionType',
     'PangoEllipsizeMode', 'PangoWrapMode']
-_BLACKLIST_ORIGINS = ['Gjs_Module', 'GtkActionable', 'GtkActivatable',
-    'GtkButton', 'GtkContainer', 'GtkGrid', 'GtkWindow']
+_BLACKLIST_ORIGINS = ['EosWindow', 'Gjs_Module', 'GtkActionable',
+    'GtkActivatable', 'GtkApplicationWindow', 'GtkButton', 'GtkContainer',
+    'GtkGrid', 'GtkWindow']
 _WHITELIST_PROPERTIES = {
     'GtkEntry': ['max-length', 'max-width-chars', 'placeholder-text',
         'width-chars'],
@@ -15,6 +16,9 @@ _WHITELIST_PROPERTIES = {
     'GtkStack': ['transition-duration', 'transition-type'],
     'GtkWidget': ['expand', 'halign', 'hexpand', 'orientation', 'valign',
         'vexpand'],
+}
+_BLACKLIST_PROPERTIES = {
+    'Window.Simple': ['expand', 'halign', 'hexpand', 'valign', 'vexpand'],
 }
 
 
@@ -116,6 +120,9 @@ class Introspector:
             return
         if (origin in _WHITELIST_PROPERTIES and
             name not in _WHITELIST_PROPERTIES[origin]):
+            return
+        if (module_name in _BLACKLIST_PROPERTIES and
+            name in _BLACKLIST_PROPERTIES[module_name]):
             return
 
         type_symbol = symbols.QualifiedSymbol(type_tokens=type_name)
